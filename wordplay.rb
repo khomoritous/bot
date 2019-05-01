d8e8b48ed09156ca3dbadbca10d3938db4412428
Monde Mampe <monde.mampe@gmail.com>
2019-05-01 22:41:39 +0200

    lägger till ranking av meningar.scanna och dela upp meningar.scanna och dela meningar i ord.

--- wordplay.rb
+++ wordplay.rb
@@ -1,2 +1,27 @@
 class WordPlay
+  def best_sentence(sentences, desired_words)
+    ranked_sentences = sentences.sort_by do |s|
+      s.words_length = (s.downcase.words - desired_words).length
+    end
+    ranked_sentences.last
+  end
 end
+
+class String
+  def sentences
+    gsub(/\n|\r/, ' ').split(/\.\s+/)
+  end
+
+
+  def words
+    scan(/\w[\w\'\-]*/)
+  end
+end
+
+p %q{
+	This is sentence splitting.
+  Sentence splitting.
+  Over many lines.
+  }.sentences[1].words[1]
+
+p "This is a test of words".words
