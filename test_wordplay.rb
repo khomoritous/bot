d7afdca64fe366e2a53ce5aa8bb43cc61c605c36
Monde Mampe <monde.mampe@gmail.com>
2019-05-12 04:11:11 +0200

    lägger till test för sentences-metod som väljer rätt mening ur textsträng.

--- test_wordplay.rb
+++ test_wordplay.rb
@@ -7,4 +7,10 @@ class TestWordPlay < MiniTest::Test
   def test_sentences
     assert_equal(['a', 'b', 'c d', 'e f g'], "a. b. c d. e f g.".sentences)
   end
+
+  def test_sentences_choosing
+    test_text = %q{Hello.This is a test of sentence separation. This is the end of the test.}
+
+    assert_equal('This is the end of the test', test_text.sentences[2])
+  end
 end
