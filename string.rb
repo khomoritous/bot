4eb321f258d9f3ecd7c552009b245efd6ec8f472
Monde Mampe <monde.mampe@gmail.com>
2019-06-13 15:22:22 +0200

    lägger till metod för uppdelning av meningar.

---
+++ string.rb
@@ -0,0 +1,9 @@
+class String
+  def sentences
+    gsub(/\n|\r/, ' ').split(/\.\s*/)
+  end
+end
+
+
+
+p %q{Hello. This is a test of basic sentence splitting. It even works over multiple lines.}.sentences
