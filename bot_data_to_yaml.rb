6048899d9a69043ce03e0bdefb28bb179085837c
Monde Mampe <monde.mampe@gmail.com>
2019-06-14 16:19:22 +0200

    lägger till fil och börjar lägga till bot data som bot har vid startup.

---
+++ bot_data_to_yaml.rb
@@ -0,0 +1,4 @@
+require 'yaml'
+
+bot_data = {:presubs => [["dont","don't"], ["youre", "you're"], ["love", "like"]], :responses => {:default
+=> ["I don't understand", "What?", "Huh?"],:greeting => ["Hi. I'm [name]. Want to chat?"],:farewell => ["Good bye!"], ["How's it going?", "How do you do?"],'hello' => }}
--- wordplay.rb
+++ wordplay.rb
@@ -1,12 +1,16 @@
-class WordPlay
-
-  def self.best_sentence(sentences, desired_words)
-    ranked_sentences = sentences.sort_by do |s|
-      s.words.length - (s.downcase.words - desired_words).length
-    end
-    ranked_sentences.last
+class String
+  def sentences
+    gsub(/\n|\r/, ' ').split(/\.\s*/)
   end
 
+
+  def words
+    scan(/\w[\w\'\-]*/)
+  end
+end
+
+
+class WordPlay
 
   def self.switch_pronouns(text)
     text.gsub(/\b(I am|You are|I|You|Me|Your|My)\b/i) do |pronoun|
@@ -29,18 +33,17 @@ class WordPlay
     end.sub(/^me\b/i, 'i')
   end
 
-end
 
-class String
-  def sentences
-    gsub(/\n|\r/, ' ').split(/\.\s*/)
+  def self.best_sentence(sentences, desired_words)
+    ranked_sentences = sentences.sort_by do |s|
+      s.words.length - (s.downcase.words - desired_words).length
+    end
+    ranked_sentences.last
   end
 
-
-  def words
-    scan(/\w[\w\'\-]*/)
-  end
 end
+
+
 
 p %q{
   This is sentence splitting.
